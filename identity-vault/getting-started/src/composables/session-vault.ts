4080da419394b7b2348d22ff1d96cf8298302554
Ken Sodemann <ken@ionic.io>
2024-01-15 12:28:41 -0600

    feat(iv): add getting started tutorial

---
+++ identity-vault/getting-started/src/composables/session-vault.ts
@@ -0,0 +1,69 @@
+import { useVaultFactory } from '@/composables/vault-factory';
+import { Session } from '@/models/session';
+import {
+  BrowserVault,
+  DeviceSecurityType,
+  IdentityVaultConfig,
+  Vault,
+  VaultType,
+} from '@ionic-enterprise/identity-vault';
+import { ref } from 'vue';
+
+export type UnlockMode = 'BiometricsWithPasscode' | 'InMemory' | 'SecureStorage';
+
+const { createVault } = useVaultFactory();
+const vault: Vault | BrowserVault = createVault();
+const session = ref<Session | null>(null);
+
+const initializeVault = async (): Promise<void> => {
+  await vault.initialize({
+    key: 'io.ionic.gettingstartediv',
+    type: VaultType.SecureStorage,
+    deviceSecurityType: DeviceSecurityType.None,
+    lockAfterBackgrounded: 2000,
+  });
+
+  vault.onLock(() => (session.value = null));
+};
+
+const storeSession = async (s: Session): Promise<void> => {
+  vault.setValue('session', s);
+  session.value = s;
+};
+
+const getSession = async (): Promise<void> => {
+  session.value = await vault.getValue<Session>('session');
+};
+
+const clearSession = async (): Promise<void> => {
+  await vault.clear();
+  session.value = null;
+};
+
+const lockSession = async (): Promise<void> => {
+  await vault.lock();
+  session.value = null;
+};
+
+const updateUnlockMode = async (mode: UnlockMode): Promise<void> => {
+  const type =
+    mode === 'BiometricsWithPasscode'
+      ? VaultType.DeviceSecurity
+      : mode === 'InMemory'
+        ? VaultType.InMemory
+        : VaultType.SecureStorage;
+  await vault.updateConfig({
+    ...(vault.config as IdentityVaultConfig),
+    type,
+  });
+};
+
+export const useSessionVault = (): any => ({
+  clearSession,
+  getSession,
+  initializeVault,
+  lockSession,
+  session,
+  storeSession,
+  updateUnlockMode,
+});
---
+++ identity-vault/getting-started/src/views/Tab1Page.vue
@@ -0,0 +1,93 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" @click="storeClicked">Store</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" color="danger" @click="clearSession">Clear</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" color="secondary" @click="updateUnlockMode('BiometricsWithPasscode')">Use
+              Biometrics</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" color="secondary" @click="updateUnlockMode('InMemory')">Use In Memory</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" color="secondary" @click="updateUnlockMode('SecureStorage')">Use Secure
+              Storage</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" color="warning" @click="lockSession">Lock</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <ion-button expand="block" color="warning" @click="getSession">Unlock</ion-button>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <div>
+            <div>{{ session?.email }}</div>
+            <div>{{ session?.firstName }} {{ session?.lastName }}</div>
+            <div>{{ session?.accessToken }}</div>
+            <div>{{ session?.refreshToken }}</div>
+          </div>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonButton,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import { useSessionVault } from '@/composables/session-vault';
+
+const { clearSession, getSession, lockSession, session, storeSession, updateUnlockMode } = useSessionVault();
+
+const storeClicked = async (): Promise<void> => {
+  await storeSession({
+    email: 'test@ionic.io',
+    firstName: 'Tessa',
+    lastName: 'Testsmith',
+    accessToken: '4abf1d79-143c-4b89-b478-19607eb5ce97',
+    refreshToken: '565111b6-66c3-4527-9238-6ea2cc017126',
+  });
+}
+
+getSession();
+</script>
