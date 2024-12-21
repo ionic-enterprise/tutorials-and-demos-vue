a2d70e98e25067e32d2bb72e045e53884ea36faf
Ken Sodemann <ken.sodemann@gmail.com>
2024-12-21 08:12:46 -0600

    improve the typings on the compare

--- demos/security-trifecta/src/composables/compare.ts
+++ demos/security-trifecta/src/composables/compare.ts
@@ -1,19 +1,24 @@
-const byStringProp = (x1: any, x2: any, propName: string): number => {
-  const str1 = x1[propName].trim().toUpperCase();
-  const str2 = x2[propName].trim().toUpperCase();
+interface BrandNamePair {
+  brand: string;
+  name: string;
+}
+
+const byStringProp = (x1: Partial<BrandNamePair>, x2: Partial<BrandNamePair>, propName: 'brand' | 'name'): number => {
+  const str1 = x1[propName]?.trim().toUpperCase() || '';
+  const str2 = x2[propName]?.trim().toUpperCase() || '';
 
   return str1.localeCompare(str2);
 };
 
-const byName = (x1: { name: string }, x2: { name: string }): number => {
+const byName = (x1: Pick<BrandNamePair, 'name'>, x2: Pick<BrandNamePair, 'name'>): number => {
   return byStringProp(x1, x2, 'name');
 };
 
-const byBrand = (x1: { brand: string }, x2: { brand: string }): number => {
+const byBrand = (x1: Pick<BrandNamePair, 'brand'>, x2: Pick<BrandNamePair, 'brand'>): number => {
   return byStringProp(x1, x2, 'brand');
 };
 
-const byBrandAndName = (x1: { brand: string; name: string }, x2: { brand: string; name: string }): number => {
+const byBrandAndName = (x1: BrandNamePair, x2: BrandNamePair): number => {
   return byBrand(x1, x2) || byName(x1, x2);
 };
 
